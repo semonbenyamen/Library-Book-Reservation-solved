60547edaeb919874442573531ef77e5a9490ec67
semonbenyamen <semobenyamen@gmail.com>
2026-03-02 21:12:30 +0200

    comment2
    Ensured proper REST design by using correct HTTP methods.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { reserveBooks, deleteBook } = require("../controllers/bookController");
 
+// Bug: Previously no structural issue here, but ensuring proper REST design
+// Fix: Keeping correct HTTP methods
 router.post("/books/reserve", reserveBooks);
 router.delete("/books/:id", deleteBook);
 
